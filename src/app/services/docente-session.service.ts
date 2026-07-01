93721322df7f09dd54ef0cbcc80be6364ad86597
DyerMandujano <dyer0206@gmail.com>
2026-06-30 19:12:33 -0500

    arreglando merge x2

--- src/app/services/docente-session.service.ts
+++ src/app/services/docente-session.service.ts
@@ -1,36 +1,10 @@
-import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocenteSessionService {
 
-<<<<<<< HEAD
-  // 1. Inyectamos PLATFORM_ID en el constructor
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-  getNombreDocente() {
-    // 2. Verificamos si estamos en el navegador antes de usar localStorage
-    if (isPlatformBrowser(this.platformId)) {
-      return localStorage.getItem('nombreDocente'); // Usa la llave que tengas definida
-    }
-    return null; // Retorna null o '' si se está renderizando en el servidor
-  }
-
-  setNombreDocente(nombre: string) {
-    if (isPlatformBrowser(this.platformId)) {
-      localStorage.setItem('nombreDocente', nombre);
-    }
-  }
-
-  // EJEMPLO: Si tienes un método para guardar o leer el token, haz lo mismo:
-  getToken() {
-    if (isPlatformBrowser(this.platformId)) {
-      return localStorage.getItem('token');
-    }
-    return null;
-=======
   private keyNombre = 'nombreDelDocente';
   private keyIdDocente = 'idDocente';
 
@@ -46,7 +20,6 @@ export class DocenteSessionService {
     if (id && id > 0) {
       localStorage.setItem(this.keyIdDocente, id.toString());
     }
->>>>>>> main
   }
 
   getIdDocente(): number {
